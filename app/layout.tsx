dccb695e53be35057cdb5983646c91bd026f30b3
Sahidul Islam Jony <jonyahmed19@gmail.com>
2024-02-04 16:22:29 +0600

    nextjs auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
-
+import '@radix-ui/themes/styles.css';
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Theme } from '@radix-ui/themes';
 
 export const metadata: Metadata = {
     title: {
@@ -42,7 +43,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 >
                     <ThemeProvider attribute="class" defaultTheme="light">
                         <div className="relative flex min-h-screen flex-col">
-                            <div className="flex-1">{children}</div>
+                            <div className="flex-1">
+                                <Theme>
+                                {children}
+                                
+                                </Theme>
+                               
+                                </div>
                         </div>
                         <TailwindIndicator />
                     </ThemeProvider>
